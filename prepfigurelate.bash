3c3b546e267c26034bfa9e6aca48b61f3559ea43
ropsoft <markcasey+ropsoft@pointofrental.com>
2016-08-11 05:20:24 +0000

    script wip

--- prepfigurelate.bash
+++ prepfigurelate.bash
@@ -9,8 +9,8 @@ source /etc/coreos/update.conf
 source /etc/os-release
 echo "This deploy host is booted to CoreOS ${VERSION} from the ${GROUP} release channel."
 echo " - Will download and deploy CoreOS ${VERSION} from release channel ${GROUP} to any target nodes needing install."
-find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_channel/${GROUP}/" {} \;
-find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_version/${VERSION}/" {} \;
+#find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_channel/${GROUP}/" {} \;
+#find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_version/${VERSION}/" {} \;
 
 # needed for next sections
 SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
@@ -19,13 +19,11 @@ SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
 echo "Your SSH session to this deploy host is to its IP address '${SSH_CONNECTION_ARRAY[2]}'; this address is assumed to be on the management network."
 export KPC_bootcfg_endpoint="${SSH_CONNECTION_ARRAY[2]}"
 echo " - Will instruct booting nodes to reach bootcfg service at ${KPC_bootcfg_endpoint}:8080 for configs and images."
-find ./bootcfg -type f -exec sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/" {} \;
+#find ./bootcfg -type f -exec sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/" {} \;
 
 # a hint for our ip-metadata-kpc systemd unit to determine which IP/interface services like etcd2 should bootstrap on
 export KPC_private_subnet_hint="${KPC_bootcfg_endpoint%.*}."
-find ./bootcfg -type f -exec sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/" {} \;
-
-exit 0
+#find ./bootcfg -type f -exec sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/" {} \;
 
 # MAY BE ADDED LATER... This is a partial implementation for manual selection option of the subnet hint/bootcfg endpoint
 # choose bootcfg endpoint and mgmt subnet hint
@@ -36,18 +34,23 @@ exit 0
 # get list of authorized_keys for the current user and pass them along to the booted nodes
 mapfile -t pubkeys_pre < ~/.ssh/authorized_keys
 pubkeys=()
+echo
 echo "WARNING WARNING WARNING"
 echo "Nodes booting from network and installing will be configured to allow the core user to SSH "
 echo "in with **ALL** of the keys found in ~/.ssh/authorized_keys. These include:"
+echo
 for key in "${pubkeys_pre[@]}"; do
-    if [[ key != '' && key !~ ^# ]]
+    if [[ "${key}" != '' && ! "${key}" =~ ^# ]]
     then
-        echo "${key}" | sed -e 's/\(.\{25\}\).*\(.\{25\}\)/\1 ... ... ... \2/'
-        pubkeys+=("${key}")
+        echo "${key}" | sed -e 's/\(.\{25\}\).*\(.\{25\}\)/ - \1 ... ... ... \2/'
+        pubkeys+=("\"${key}\",")
     fi
 done
+echo
 
-export KPC_ssh_authorized_keys="$( echo ${pubkeys[*] | sed -e 's/\ /\", \"/')"
+pubkeys_string_temp="${pubkeys[@]}"
+export KPC_ssh_authorized_keys="${pubkeys_string_temp%,}"
+
 # sed delimiter changed to avoid escaping '/'
 find ./bootcfg -type f -exec sed -i -e "s|KPC_ssh_authorized_keys|${KPC_ssh_authorized_keys}|" {} \;
 
