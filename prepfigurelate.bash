3e37231fe3a8b286062d80d33bcfe93f1199bafd
ropsoft <markcasey+ropsoft@pointofrental.com>
2016-08-11 04:35:39 +0000

    fix a var, only look in bootcfg dir for templating

--- prepfigurelate.bash
+++ prepfigurelate.bash
@@ -8,9 +8,9 @@
 source /etc/coreos/update.conf
 source /etc/os-release
 echo "This deploy host is booted to CoreOS ${VERSION} from the ${GROUP} release channel."
-echo " - Will download and deploy CoreOS {$VERSION} from release channel ${GROUP} to any target nodes needing install."
-find ./ -type f -exec sed -i -e "s/KPC_coreos_channel/${GROUP}/" {} \;
-find ./ -type f -exec sed -i -e "s/KPC_coreos_version/${VERSION}/" {} \;
+echo " - Will download and deploy CoreOS ${VERSION} from release channel ${GROUP} to any target nodes needing install."
+find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_channel/${GROUP}/" {} \;
+find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_version/${VERSION}/" {} \;
 
 # needed for next sections
 SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
@@ -19,11 +19,13 @@ SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
 echo "Your SSH session to this deploy host is to its IP address '${SSH_CONNECTION_ARRAY[2]}'; this address is assumed to be on the management network."
 export KPC_bootcfg_endpoint="${SSH_CONNECTION_ARRAY[2]}"
 echo " - Will instruct booting nodes to reach bootcfg service at ${KPC_bootcfg_endpoint}:8080 for configs and images."
-find ./ -type f -exec sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/" {} \;
+find ./bootcfg -type f -exec sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/" {} \;
 
 # a hint for our ip-metadata-kpc systemd unit to determine which IP/interface services like etcd2 should bootstrap on
 export KPC_private_subnet_hint="${KPC_bootcfg_endpoint%.*}."
-find ./ -type f -exec sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/" {} \;
+find ./bootcfg -type f -exec sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/" {} \;
+
+exit 0
 
 # MAY BE ADDED LATER... This is a partial implementation for manual selection option of the subnet hint/bootcfg endpoint
 # choose bootcfg endpoint and mgmt subnet hint
@@ -47,12 +49,12 @@ done
 
 export KPC_ssh_authorized_keys="$( echo ${pubkeys[*] | sed -e 's/\ /\", \"/')"
 # sed delimiter changed to avoid escaping '/'
-find ./ -type f -exec sed -i -e "s|KPC_ssh_authorized_keys|${KPC_ssh_authorized_keys}|" {} \;
+find ./bootcfg -type f -exec sed -i -e "s|KPC_ssh_authorized_keys|${KPC_ssh_authorized_keys}|" {} \;
 
 
 
 # create a token to bootstrap etcd - remember to set size to the number of target nodes
 export KPC_discovery_token="$(curl -w "\n" 'https://discovery.etcd.io/new?size=3')"
 # sed delimiter changed to avoid escaping '/'
-find ./ -type f -exec sed -i -e "s|KPC_discovery_token|${KPC_discovery_token}|" {} \;
+find ./bootcfg -type f -exec sed -i -e "s|KPC_discovery_token|${KPC_discovery_token}|" {} \;
 
