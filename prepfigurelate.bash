bf0c5eb070e536c060aeded94e08a93f7a407acf
ropsoft <markcasey+ropsoft@pointofrental.com>
2016-08-11 05:27:33 +0000

    uncomment

--- prepfigurelate.bash
+++ prepfigurelate.bash
@@ -9,8 +9,8 @@ source /etc/coreos/update.conf
 source /etc/os-release
 echo "This deploy host is booted to CoreOS ${VERSION} from the ${GROUP} release channel."
 echo " - Will download and deploy CoreOS ${VERSION} from release channel ${GROUP} to any target nodes needing install."
-#find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_channel/${GROUP}/" {} \;
-#find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_version/${VERSION}/" {} \;
+find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_channel/${GROUP}/" {} \;
+find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_version/${VERSION}/" {} \;
 
 # needed for next sections
 SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
@@ -19,11 +19,11 @@ SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
 echo "Your SSH session to this deploy host is to its IP address '${SSH_CONNECTION_ARRAY[2]}'; this address is assumed to be on the management network."
 export KPC_bootcfg_endpoint="${SSH_CONNECTION_ARRAY[2]}"
 echo " - Will instruct booting nodes to reach bootcfg service at ${KPC_bootcfg_endpoint}:8080 for configs and images."
-#find ./bootcfg -type f -exec sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/" {} \;
+find ./bootcfg -type f -exec sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/" {} \;
 
 # a hint for our ip-metadata-kpc systemd unit to determine which IP/interface services like etcd2 should bootstrap on
 export KPC_private_subnet_hint="${KPC_bootcfg_endpoint%.*}."
-#find ./bootcfg -type f -exec sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/" {} \;
+find ./bootcfg -type f -exec sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/" {} \;
 
 # MAY BE ADDED LATER... This is a partial implementation for manual selection option of the subnet hint/bootcfg endpoint
 # choose bootcfg endpoint and mgmt subnet hint
