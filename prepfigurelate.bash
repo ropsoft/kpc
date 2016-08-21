8646719a9c6036612daab31dcd2c861a00cdaa32
CoreOS Admin <markcasey+ropsoft@pointofrental.com>
2016-08-21 06:13:40 +0000

    make templating faster with grep instead of looking in all files

--- prepfigurelate.bash
+++ prepfigurelate.bash
@@ -4,14 +4,18 @@
 # Require git working copy to be clean so we can template with immunity...
 # If not clean recommend a git reset (with a data loss warning), then exit 1
 
+# possible breakage if script is run via a symlink (which is not the case as-shipped)
+# the common pattern to resolve the actual path is ugly and very confusing and not worth covering this corner case
+this_script="$(basename $0)"
+
 # source some files to determine CoreOS channel and version, then sub them in
 [ -e /usr/share/coreos/update.conf ] && source /usr/share/coreos/update.conf
 [ -e /etc/coreos/update.conf ] && source /etc/coreos/update.conf
 source /etc/os-release
 echo "This deploy host is booted to CoreOS ${VERSION} from the ${GROUP} release channel."
 echo " - Will download and deploy CoreOS ${VERSION} from release channel ${GROUP} to any target nodes needing install."
-find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_channel/${GROUP}/" {} \;
-find ./bootcfg -type f -exec sed -i -e "s/KPC_coreos_version/${VERSION}/" {} \;
+grep -rlZ KPC_coreos_channel ./bootcfg | grep -zZv "${this_script}" | xargs -0 sed -i -e "s/KPC_coreos_channel/${GROUP}/"
+grep -rlZ KPC_coreos_version ./bootcfg | grep -zZv "${this_script}" | xargs -0 sed -i -e "s/KPC_coreos_version/${VERSION}/"
 
 # needed for next sections
 SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
@@ -20,11 +24,11 @@ SSH_CONNECTION_ARRAY=( ${SSH_CONNECTION} )
 echo "Your SSH session to this deploy host is to its IP address '${SSH_CONNECTION_ARRAY[2]}'; this address is assumed to be on the management network."
 export KPC_bootcfg_endpoint="${SSH_CONNECTION_ARRAY[2]}"
 echo " - Will instruct booting nodes to reach bootcfg service at ${KPC_bootcfg_endpoint}:8080 for configs and images."
-find ./bootcfg -type f -exec sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/" {} \;
+grep -rlZ KPC_bootcfg_endpoint ./bootcfg | grep -zZv "${this_script}" | xargs -0 sed -i -e "s/KPC_bootcfg_endpoint/${KPC_bootcfg_endpoint}/"
 
 # a hint for our ip-metadata-kpc systemd unit to determine which IP/interface services like etcd2 should bootstrap on
 export KPC_private_subnet_hint="${KPC_bootcfg_endpoint%.*}."
-find ./bootcfg -type f -exec sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/" {} \;
+grep -rlZ KPC_private_subnet_hint ./bootcfg | grep -zZv "${this_script}" | xargs -0 sed -i -e "s/KPC_private_subnet_hint/${KPC_private_subnet_hint}/"
 
 # MAY BE ADDED LATER... This is a partial implementation for manual selection option of the subnet hint/bootcfg endpoint
 # choose bootcfg endpoint and mgmt subnet hint
@@ -59,7 +63,7 @@ pubkeys_string_temp="${pubkeys[@]}"
 export KPC_ssh_authorized_keys="${pubkeys_string_temp%,}"
 
 # sed delimiter changed to avoid escaping '/'
-find ./bootcfg -type f -exec sed -i -e "s|KPC_ssh_authorized_keys|${KPC_ssh_authorized_keys}|" {} \;
+grep -rlZ KPC_ssh_authorized_keys ./bootcfg | grep -zZv "${this_script}" | xargs -0 sed -i -e "s|KPC_ssh_authorized_keys|${KPC_ssh_authorized_keys}|"
 
 
 
@@ -70,7 +74,9 @@ export KPC_discovery_token="$(curl -w "\n" 'https://discovery.etcd.io/new?size=3
 echo "Got ${KPC_discovery_token}"
 #FIXME error out if no token
 # sed delimiter changed to avoid escaping '/'
-find ./bootcfg -type f -exec sed -i -e "s|KPC_discovery_token|${KPC_discovery_token##*/}|" {} \;
+grep -rlZ KPC_discovery_token ./bootcfg | grep -zZv "${this_script}" | xargs -0 sed -i -e "s|KPC_discovery_token|${KPC_discovery_token##*/}|"
 
 echo "Downloading required coreos images using upstream get-coreos script"
 bootcfg/scripts/get-coreos "${GROUP}" "${VERSION}" ./bootcfg/assets
+
+
