9e2e11281f1aaed62a44f5da3e04df4762a42f3d
ropsoft <ropsoft@users.noreply.github.com>
2016-08-29 01:19:04 -0500

    Update template-bootcfg.bash

--- template-bootcfg.bash
+++ template-bootcfg.bash
@@ -2,6 +2,25 @@
 
 this_script="$(basename $0)"
 
+# next two functions:
+# http://stackoverflow.com/questions/20762575/explanation-of-convertor-of-cidr-to-netmask-in-linux-shell-netmask2cdir-and-cdir
+mask2cdr ()
+{
+   # Assumes there's no "255." after a non-255 byte in the mask
+   local x=${1##*255.}
+   set -- 0^^^128^192^224^240^248^252^254^ $(( (${#1} - ${#x})*2 )) ${x%%.*}
+   x=${1%%$3*}
+   echo $(( $2 + (${#x}/4) ))
+}
+
+cdr2mask ()
+{
+   # Number of args to shift, 255..255, first non-255 byte, zeroes
+   set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
+   [ $1 -gt 1 ] && shift $1 || shift
+   echo ${1-0}.${2-0}.${3-0}.${4-0}
+}
+
 if [[ "$(git rev-parse --show-toplevel)" != "${PWD}" ]]
 then
     echo "ERROR: ${this_script} cannot continue"
